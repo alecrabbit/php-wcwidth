c926b4ff11f88c85f307b4b82bcf457e4b48e4ce
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-09-18 14:56:09 +0300

    wp 3

--- tests/Unit/WcWidthTest.php
+++ tests/Unit/WcWidthTest.php
@@ -17,22 +17,56 @@ class WcWidthTest extends TestCase
         $this->validate($expect_length_each, $expect_length_phrase, $phrase);
     }
 
+    protected function validate(array $expect_length_each, int $expect_length_phrase, string $phrase): void
+    {
+        $chrArray = preg_split('//u', $phrase, -1, PREG_SPLIT_NO_EMPTY);
+        $length_each = array_map([WcWidth::class, 'wcwidth'], $chrArray);
+        $length_phrase = WcWidth::wcswidth($phrase, mb_strlen($phrase));
+
+        $this->assertSame($length_each, $expect_length_each);
+        $this->assertSame($length_phrase, $expect_length_phrase);
+    }
+
     /** @test */
     public function nullWidthZero(): void
     {
+        // NULL (0) reports width 0.
         $phrase = "abc\x00def";
         $expect_length_each = [1, 1, 1, 0, 1, 1, 1];
         $expect_length_phrase = array_sum($expect_length_each);
 
         $this->validate($expect_length_each, $expect_length_phrase, $phrase);
     }
-
+    
     /** @test */
     public function csiWithNegative(): void
     {
+        // CSI (Control sequence initiate) reports width -1.
         $phrase = "\x1b[0m";
         $expect_length_each = [-1, 1, 1, 1];
         $expect_length_phrase = -1;
+
+        $this->validate($expect_length_each, $expect_length_phrase, $phrase);
+    }
+
+    /** @test */
+    public function combiningWidthNegative(): void
+    {
+        // Simple test combining reports total width of 4.
+        $phrase = "--\u{05bf}--";
+        $expect_length_each = [1, 1, 0, 1, 1];
+        $expect_length_phrase = 4;
+
+        $this->validate($expect_length_each, $expect_length_phrase, $phrase);
+    }
+
+    /** @test */
+    public function combiningWidthAcute(): void
+    {
+        // Phrase cafe + COMBINING ACUTE ACCENT is café of length 4.
+        $phrase = "cafe\u{0301}";
+        $expect_length_each = [1, 1, 1, 1, 0];
+        $expect_length_phrase = 4;
 
         $this->validate($expect_length_each, $expect_length_phrase, $phrase);
     }
@@ -47,15 +81,5 @@ class WcWidthTest extends TestCase
         $length_phrase = WcWidth::wcswidth($phrase, 7);
         $this->assertSame($length_phrase, $expect_length_phrase);
 
-    }
-
-    protected function validate(array $expect_length_each, int $expect_length_phrase, string $phrase): void
-    {
-        $chrArray = preg_split('//u', $phrase, -1, PREG_SPLIT_NO_EMPTY);
-        $length_each = array_map([WcWidth::class, 'wcwidth'], $chrArray);
-        $length_phrase = WcWidth::wcswidth($phrase, mb_strlen($phrase));
-
-        $this->assertSame($length_each, $expect_length_each);
-        $this->assertSame($length_phrase, $expect_length_phrase);
     }
 }
