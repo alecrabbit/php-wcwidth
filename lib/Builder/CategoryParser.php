95c82429d1adbdb9715a3174bb8fa7523c113b67
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-02-28 18:55:27 +0200

    ~

--- lib/Builder/CategoryParser.php
+++ lib/Builder/CategoryParser.php
@@ -18,23 +18,22 @@ final class CategoryParser implements ICategoryParser
         $result = [];
         foreach (explode(PHP_EOL, $data) as $line) {
             $l = [];
-            [$data, $_, $comment] = $this->partition($line, '#');
-//            dump(sprintf('Data:%s Comment:%s', $data, $comment));
+            [$data, $comment] = $this->split($line);
             $data_fields = explode(';', $data);
             if ($data_fields[1] ?? false) {
                 $codepoints_str = \trim($data_fields[0] ?? '');
                 $properties = \trim($data_fields[1] ?? '');
-                if(in_array($properties, $categories, true)) {
-                    if(str_contains($codepoints_str, '..')) {
+                if (in_array($properties, $categories, true)) {
+                    if (str_contains($codepoints_str, '..')) {
                         [$start, $end] = explode('..', $codepoints_str);
                     } else {
                         $start = $end = $codepoints_str;
                     }
-                    $l[0] = [$start, $end];
+                    $l[0] = [hexdec($start), hexdec($end)];
                     $l[1] = $properties;
                 }
             }
-            if($l[0] ?? false) {
+            if ($l[0] ?? false) {
                 $l[2] = $comment;
                 $result[] = $l;
             }
@@ -42,16 +41,15 @@ final class CategoryParser implements ICategoryParser
         return $result;
     }
 
-    private function partition(string $line, string $separator): array
+    private function split(string $line): array
     {
-        $pos = strpos($line, $separator);
+        $pos = strpos($line, '#');
         if (false === $pos) {
-            return [$line, '', ''];
+            return [$line, ''];
         }
         return [
             trim(substr($line, 0, $pos)),
-            $separator,
-            trim(substr($line, $pos + strlen($separator))),
+            trim(substr($line, $pos + 1)),
         ];
     }
 }
--- tests/Unit/Builder/CategoryParserTest.php
+++ tests/Unit/Builder/CategoryParserTest.php
@@ -10,6 +10,7 @@ use PHPUnit\Framework\Attributes\Test;
 class CategoryParserTest extends TestCase
 {
     private const WIDE_CATEGORIES = ['W', 'F'];
+    private const ZERO_CATEGORIES = ['Me', 'Mn'];
     private CategoryParser $parser;
 
     public static function parseCategoryData(): iterable
@@ -22,6 +23,24 @@ class CategoryParserTest extends TestCase
                         [0x01100, 0x0115f,],
                         'W',
                         'Lo    [96] HANGUL CHOSEONG KIYEOK..HANGUL CHOSEONG FILLER',
+                    ],
+                    [
+                        // 231A..231B;W     # So     [2] WATCH..HOURGLASS
+                        [0x0231a, 0x0231b,],
+                        'W',
+                        'So     [2] WATCH..HOURGLASS',
+                    ],
+                    [
+                        // 2329;W           # Ps         LEFT-POINTING ANGLE BRACKET
+                        [0x02329, 0x02329,],
+                        'W',
+                        'Ps         LEFT-POINTING ANGLE BRACKET',
+                    ],
+                    [
+                        // 232A;W           # Pe         RIGHT-POINTING ANGLE BRACKET
+                        [0x0232a, 0x0232a,],
+                        'W',
+                        'Pe         RIGHT-POINTING ANGLE BRACKET',
                     ],
                 ],
             ],
@@ -95,6 +114,154 @@ class CategoryParserTest extends TestCase
                         232B..237B;N     # So    [81] ERASE TO THE LEFT..NOT CHECK MARK
                         DATA,
                     self::CATEGORIES => self::WIDE_CATEGORIES,
+                ],
+            ],
+        ];
+        yield [
+            [
+                self::RESULT => [
+                    [
+                        //  0300..036F    ; Mn # [112] COMBINING GRAVE ACCENT..COMBINING LATIN SMALL LETTER X
+                        [0x0300, 0x036F,],
+                        'Mn',
+                        '[112] COMBINING GRAVE ACCENT..COMBINING LATIN SMALL LETTER X',
+                    ],
+                    [
+                        // 0483..0487    ; Mn #   [5] COMBINING CYRILLIC TITLO..COMBINING CYRILLIC POKRYTIE
+                        [0x0483, 0x0487,],
+                        'Mn',
+                        '[5] COMBINING CYRILLIC TITLO..COMBINING CYRILLIC POKRYTIE',
+                    ],
+                    [
+                        // 0591..05BD    ; Mn #  [45] HEBREW ACCENT ETNAHTA..HEBREW POINT METEG
+                        [0x0591, 0x05BD,],
+                        'Mn',
+                        '[45] HEBREW ACCENT ETNAHTA..HEBREW POINT METEG',
+                    ],
+                    [
+                        // 1E944..1E94A  ; Mn #   [7] ADLAM ALIF LENGTHENER..ADLAM NUKTA
+                        [0x1E944, 0x1E94A,],
+                        'Mn',
+                        '[7] ADLAM ALIF LENGTHENER..ADLAM NUKTA',
+                    ],
+                    [
+                        // E0100..E01EF  ; Mn # [240] VARIATION SELECTOR-17..VARIATION SELECTOR-256
+                        [0xE0100, 0xE01EF,],
+                        'Mn',
+                        '[240] VARIATION SELECTOR-17..VARIATION SELECTOR-256',
+                    ],
+                    [
+                        // 0488..0489    ; Me #   [2] COMBINING CYRILLIC HUNDRED THOUSANDS SIGN..COMBINING CYRILLIC MILLIONS SIGN
+                        [0x0488, 0x0489,],
+                        'Me',
+                        '[2] COMBINING CYRILLIC HUNDRED THOUSANDS SIGN..COMBINING CYRILLIC MILLIONS SIGN',
+                    ],
+                    [
+                        // 1ABE          ; Me #       COMBINING PARENTHESES OVERLAY
+                        [0x1ABE, 0x1ABE,],
+                        'Me',
+                        'COMBINING PARENTHESES OVERLAY',
+                    ],
+                    [
+                        // 20DD..20E0    ; Me #   [4] COMBINING ENCLOSING CIRCLE..COMBINING ENCLOSING CIRCLE BACKSLASH
+                        [0x20DD, 0x20E0,],
+                        'Me',
+                        '[4] COMBINING ENCLOSING CIRCLE..COMBINING ENCLOSING CIRCLE BACKSLASH',
+                    ],
+                    [
+                        // 20E2..20E4    ; Me #   [3] COMBINING ENCLOSING SCREEN..COMBINING ENCLOSING UPWARD POINTING TRIANGLE
+                        [0x20E2, 0x20E4,],
+                        'Me',
+                        '[3] COMBINING ENCLOSING SCREEN..COMBINING ENCLOSING UPWARD POINTING TRIANGLE',
+                    ],
+                    [
+                        // A670..A672    ; Me #   [3] COMBINING CYRILLIC TEN MILLIONS SIGN..COMBINING CYRILLIC THOUSAND MILLIONS SIGN
+                        [0xA670, 0xA672,],
+                        'Me',
+                        '[3] COMBINING CYRILLIC TEN MILLIONS SIGN..COMBINING CYRILLIC THOUSAND MILLIONS SIGN',
+                    ],
+                ],
+            ],
+            [
+                self::ARGUMENTS => [
+                    self::DATA =>
+                        <<<'DATA'
+                        # DerivedGeneralCategory-14.0.0.txt
+                        # Date: 2021-07-10, 00:35:08 GMT
+                        # © 2021 Unicode®, Inc.
+                        # Unicode and the Unicode Logo are registered trademarks of Unicode, Inc. in the U.S. and other countries.
+                        # For terms of use, see http://www.unicode.org/terms_of_use.html
+                        #
+                        # Unicode Character Database
+                        #   For documentation, see http://www.unicode.org/reports/tr44/
+                        
+                        # ================================================
+                        
+                        # Property:	General_Category
+                        
+                        # ================================================
+                        
+                        # General_Category=Unassigned
+                        
+                        0378..0379    ; Cn #   [2] <reserved-0378>..<reserved-0379>
+                        0380..0383    ; Cn #   [4] <reserved-0380>..<reserved-0383>
+                        038B          ; Cn #       <reserved-038B>
+                        038D          ; Cn #       <reserved-038D>
+                        E01F0..EFFFF  ; Cn # [65040] <reserved-E01F0>..<noncharacter-EFFFF>
+                        FFFFE..FFFFF  ; Cn #   [2] <noncharacter-FFFFE>..<noncharacter-FFFFF>
+                        10FFFE..10FFFF; Cn #   [2] <noncharacter-10FFFE>..<noncharacter-10FFFF>
+                        
+                        # Total code points: 829834
+                        
+                        # ================================================
+                        
+                        # General_Category=Uppercase_Letter
+                        
+                        0041..005A    ; Lu #  [26] LATIN CAPITAL LETTER A..LATIN CAPITAL LETTER Z
+                        00C0..00D6    ; Lu #  [23] LATIN CAPITAL LETTER A WITH GRAVE..LATIN CAPITAL LETTER O WITH DIAERESIS
+                        00D8..00DE    ; Lu #   [7] LATIN CAPITAL LETTER O WITH STROKE..LATIN CAPITAL LETTER THORN
+                        0100          ; Lu #       LATIN CAPITAL LETTER A WITH MACRON
+                        0102          ; Lu #       LATIN CAPITAL LETTER A WITH BREVE
+                        0104          ; Lu #       LATIN CAPITAL LETTER A WITH OGONEK
+                        2F800..2FA1D  ; Lo # [542] CJK COMPATIBILITY IDEOGRAPH-2F800..CJK COMPATIBILITY IDEOGRAPH-2FA1D
+                        30000..3134A  ; Lo # [4939] CJK UNIFIED IDEOGRAPH-30000..CJK UNIFIED IDEOGRAPH-3134A
+                        
+                        # Total code points: 127333
+                        
+                        # ================================================
+                        
+                        # General_Category=Nonspacing_Mark
+                        
+                        0300..036F    ; Mn # [112] COMBINING GRAVE ACCENT..COMBINING LATIN SMALL LETTER X
+                        0483..0487    ; Mn #   [5] COMBINING CYRILLIC TITLO..COMBINING CYRILLIC POKRYTIE
+                        0591..05BD    ; Mn #  [45] HEBREW ACCENT ETNAHTA..HEBREW POINT METEG
+                        1E944..1E94A  ; Mn #   [7] ADLAM ALIF LENGTHENER..ADLAM NUKTA
+                        E0100..E01EF  ; Mn # [240] VARIATION SELECTOR-17..VARIATION SELECTOR-256
+                        
+                        # Total code points: 1950
+                        
+                        # ================================================
+                        
+                        # General_Category=Enclosing_Mark
+                        
+                        0488..0489    ; Me #   [2] COMBINING CYRILLIC HUNDRED THOUSANDS SIGN..COMBINING CYRILLIC MILLIONS SIGN
+                        1ABE          ; Me #       COMBINING PARENTHESES OVERLAY
+                        20DD..20E0    ; Me #   [4] COMBINING ENCLOSING CIRCLE..COMBINING ENCLOSING CIRCLE BACKSLASH
+                        20E2..20E4    ; Me #   [3] COMBINING ENCLOSING SCREEN..COMBINING ENCLOSING UPWARD POINTING TRIANGLE
+                        A670..A672    ; Me #   [3] COMBINING CYRILLIC TEN MILLIONS SIGN..COMBINING CYRILLIC THOUSAND MILLIONS SIGN
+                        
+                        # Total code points: 13
+                        
+                        # ================================================
+                        
+                        # General_Category=Spacing_Mark
+                        
+                        0903          ; Mc #       DEVANAGARI SIGN VISARGA
+                        093B          ; Mc #       DEVANAGARI VOWEL SIGN OOE
+                        093E..0940    ; Mc #   [3] DEVANAGARI VOWEL SIGN AA..DEVANAGARI VOWEL SIGN II
+                        0949..094C    ; Mc #   [4] DEVANAGARI VOWEL SIGN CANDRA O..DEVANAGARI
+                        DATA,
+                    self::CATEGORIES => self::ZERO_CATEGORIES,
                 ],
             ],
         ];
