be40755a090d8cead68fa364b7772cdd1ca400fd
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-02-28 16:16:51 +0200

    ~

--- lib/update.php
+++ lib/update.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../tests/bootstrap.php';
 
 $builder = new AlecRabbit\WCWidth\Builder\TablesBuilder();
 
