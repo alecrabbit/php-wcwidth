9b1c58e2e49d5967b23381dc1af738d50338f1a8
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-05 11:27:59 +0200

    ~

--- .make/.bin/gitattributes.sh
+++ .make/.bin/gitattributes.sh
@@ -13,7 +13,6 @@ IGNORES=$(awk '{print $1}' "$IGNORE_FILE" | sed 's|^/||')
 # Get the contents of the directory and sort them alphabetically
 CONTENTS=$(find "$DIRECTORY" -mindepth 1 -maxdepth 1 -exec basename {} \; | sort)
 
-
 # Create an empty .gitattributes file
 >"$DIRECTORY/.gitattributes"
 
@@ -21,15 +20,17 @@ CONTENTS=$(find "$DIRECTORY" -mindepth 1 -maxdepth 1 -exec basename {} \; | sort
 while read -r item; do
   # Remove leading forward slash from item path
   item=${item#/}
-  # Check if the item is not in the list of files and directories to exclude
-  if [[ ! "${IGNORES[*]}" =~ "${item}" ]]; then
-    if [[ ! "${EXCLUDES[*]}" =~ "${item}" ]]; then
-      # Check if the item is a directory
-      if [[ -d "$item" ]]; then
-        # If the item is a directory, add it to the .gitattributes file with the "export-ignore" attribute
-        echo "$item/ export-ignore" >>"$DIRECTORY/.gitattributes"
-      else
-        # If the item is a file, add it to the .gitattributes file with the "export-ignore" attribute
+  if [[ -d "$item" ]]; then
+    # Add a trailing slash to the directory name
+    item="$item/"
+    if [[ ! "${IGNORES[*]}" =~ ${item} ]]; then
+      if [[ ! "${EXCLUDES[*]}" =~ ${item} ]]; then
+        echo "$item export-ignore" >>"$DIRECTORY/.gitattributes"
+      fi
+    fi
+  else
+    if [[ ! "${IGNORES[*]}" =~ ${item} ]]; then
+      if [[ ! "${EXCLUDES[*]}" =~ ${item} ]]; then
         echo "$item export-ignore" >>"$DIRECTORY/.gitattributes"
       fi
     fi
