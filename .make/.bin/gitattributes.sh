47f78d12ff014143542b37ea38eb9ca865de603a
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-06 11:16:06 +0200

    ~

--- .make/.bin/gitattributes.sh
+++ .make/.bin/gitattributes.sh
@@ -3,9 +3,11 @@
 function add_entry {
   local item="$1"
   local file="$2"
+  local ignores="$3"
+  local excludes="$4"
   # Remove leading forward slash from item path
   item=${item#/}
-  if [[ ! "${IGNORES[*]}" =~ ${item} ]] && [[ ! "${EXCLUDES[*]}" =~ ${item} ]]; then
+  if [[ ! "${ignores[*]}" =~ ${item} ]] && [[ ! "${excludes[*]}" =~ ${item} ]]; then
       echo "$item export-ignore" >>"$file"
   fi
 }
@@ -13,6 +15,7 @@ function add_entry {
 DIRECTORY=${1:-$(pwd)}
 KEEP_FILE=".gitattributes.keep"
 IGNORE_FILE=".gitignore"
+FILE="$DIRECTORY/.gitattributes"
 
 # Get the list of files and directories to exclude
 EXCLUDES=$(awk '{print $1}' "$KEEP_FILE" | sed 's|^\.*/\{0,1\}||')
@@ -23,14 +26,14 @@ IGNORES=$(awk '{print $1}' "$IGNORE_FILE" | sed 's|^\.*/\{0,1\}||')
 # Get the contents of the directory and sort them alphabetically
 CONTENTS=$(find "$DIRECTORY" -mindepth 1 -maxdepth 1 -exec basename {} \; | sort)
 
-# Create an empty .gitattributes file
->"$DIRECTORY/.gitattributes"
+# Create an empty file
+> "$FILE"
 
 # Loop over each file or directory in the contents
 while read -r item; do
   if [[ -d "$item" ]]; then
     item="$item/"
   fi
-  add_entry "$item" "$DIRECTORY/.gitattributes"
+  add_entry "$item" "$FILE" "$IGNORES" "$EXCLUDES"
 done <<<"$CONTENTS"
 
