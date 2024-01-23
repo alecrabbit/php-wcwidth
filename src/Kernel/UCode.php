fa0922b97d93a9298a290b85f510d84f7ca16610
Oleksii <11045453+alecrabbit@users.noreply.github.com>
2024-01-23 17:29:22 +0200

    ~cleanup

--- src/Kernel/UCode.php
+++ src/Kernel/UCode.php
@@ -48,20 +48,24 @@ class UCode
     public static function wcswidth(string $subject, ?int $n = null, ?string $version = null): int
     {
         $end = $n ?? mb_strlen($subject);
-        $chrArray = array_slice(
+
+        $characters = array_slice(
             static::split($subject),
             0,
             $end
         );
-        $width = 0;
-        foreach ($chrArray as $char) {
-            $wcw = static::wcwidth($char, $version);
-            if ($wcw < 0) {
+
+        $sWidth = 0;
+
+        foreach ($characters as $char) {
+            $w = static::wcwidth($char, $version);
+            if ($w < 0) {
                 return -1;
             }
-            $width += $wcw;
+            $sWidth += $w;
         }
-        return $width;
+
+        return $sWidth;
     }
 
     /**
@@ -70,12 +74,14 @@ class UCode
     protected static function split(string $subject): array
     {
         $_split = preg_split('//u', $subject, -1, PREG_SPLIT_NO_EMPTY);
+
         // @codeCoverageIgnoreStart
         if (!is_array($_split)) {
             // Should never happen
             throw new RuntimeException('Failed to split string.');
         }
         // @codeCoverageIgnoreEnd
+
         return $_split;
     }
 
@@ -95,7 +101,7 @@ class UCode
         }
 
         # combining characters with zero width
-        if (static::binarySearch($ucs, ZERO_WIDTH[$version])) {
+        if (static::binarySearch($ucs, ZERO_WIDTH[$version]) === 1) {
             return 0;
         }
 
@@ -105,22 +111,24 @@ class UCode
 
     protected static function binarySearch(int $ucs, array $table): int
     {
-        $lbound = 0;
-        $ubound = count($table) - 1;
+        $lBound = 0;
+        $uBound = count($table) - 1;
 
-        if ($ucs < $table[0][0] || $ucs > $table[$ubound][1]) {
+        if ($ucs < $table[0][0] || $ucs > $table[$uBound][1]) {
             return 0;
         }
-        while ($ubound >= $lbound) {
-            $mid = ($lbound + $ubound); // 2
+
+        while ($uBound >= $lBound) {
+            $mid = ($lBound + $uBound);
             if ($ucs > $table[$mid][1]) {
-                $lbound = $mid + 1;
+                $lBound = $mid + 1;
             } elseif ($ucs < $table[$mid][0]) {
-                $ubound = $mid - 1;
+                $uBound = $mid - 1;
             } else {
                 return 1;
             }
         }
+
         return 0;
     }
 
